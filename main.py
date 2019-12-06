ab1c1064ea9e0b16ffb65123e2cd9c79c5e2a046
Akash Singh [h3ck4rz] <akashkumarsngh9@gmail.com>
2019-12-06 12:51:04 +0530

    #4-base-design: Added dir and base files  that wil be added in Project

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 
+import src.libs.schedule as sc
+import  time
+import  os
+
+def jobfunc():
+    print("Job Done ")
+
+def demo():
+    sc.every(1).seconds.do(jobfunc)
+    while True:
+        sc.run_pending()
+        time.sleep(1)
+        print(os.getppid())
+
 def main():
-    print('main function')
+    demo()
+
 
 if __name__ == "__main__":
     main()
